7b8222c563754e191ae9f9c7a293c74f48fa703e
Paulo Silva <79770252+P4ul0Silva@users.noreply.github.com>
2022-03-23 10:16:34 -0300

    Update
    additional implementations to dark mode and restart button.

--- script.js
+++ script.js
@@ -34,6 +34,8 @@ function drawFood() {
 
 
 
+
+
 document.addEventListener('keydown', update);
 
 function update (event) {
@@ -67,8 +69,7 @@ function iniciarJogo(){
 
 for(i = 1; i < snake.length; i++) {
     if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
-        clearInterval(jogo)
-        alert('Game Over :/');
+        clearInterval(jogo)        
     }
 }
 
